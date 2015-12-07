c642d7fcfd43ae952e8cde999a0df1b252a4704f
harald pichler <harald@dev>
2015-12-07 22:34:41 +0100

    use node-js 0.10.41

--- meteor-debianx64-setup.sh
+++ meteor-debianx64-setup.sh
@@ -4,13 +4,13 @@ apt-get install -y curl aptitude git mc sudo
 apt-get install -y python-software-properties
 apt-get install -y build-essential mongodb
 
-wget https://nodejs.org/dist/v0.10.36/node-v0.10.36-linux-x64.tar.gz
-tar xfz node-v0.10.36-linux-x64.tar.gz
+wget https://nodejs.org/dist/v0.10.41/node-v0.10.41-linux-x64.tar.gz
+tar xfz node-v0.10.41-linux-x64.tar.gz
 
-cp -rf node-v0.10.36-linux-x64/bin/* /usr/bin
-cp -rf node-v0.10.36-linux-x64/include/* /usr/include
-cp -rf node-v0.10.36-linux-x64/lib/* /usr/lib
-cp -rf node-v0.10.36-linux-x64/share/* /usr/share
+cp -rf node-v0.10.41-linux-x64/bin/* /usr/bin
+cp -rf node-v0.10.41-linux-x64/include/* /usr/include
+cp -rf node-v0.10.41-linux-x64/lib/* /usr/lib
+cp -rf node-v0.10.41-linux-x64/share/* /usr/share
 
 npm install -g forever
 
